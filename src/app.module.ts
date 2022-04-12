0170b8fa30288cc992318737a445a39f17098bae
teeto11 <elteeto11@gmail.com>
2022-04-12 09:52:06 +0100

    feat(master): configure graphql setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GraphQLModule, Query, Resolver } from '@nestjs/graphql';
+import { ApolloDriver } from '@nestjs/apollo';
 
+@Resolver()
+export class TempResolver {
+  @Query(() => String)
+  sayHello() {
+    return 'hello';
+  }
+}
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot({
+      driver: ApolloDriver,
+      playground: true,
+      debug: true,
+      autoSchemaFile:'lib/graphql/schema.graphql'
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService,TempResolver],
 })
-export class AppModule {}
+export class AppModule { }
