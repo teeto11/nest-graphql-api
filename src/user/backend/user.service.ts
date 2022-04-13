f6f8e7c2e66a41ae9f5ebb09c3a87c0316b86f71
teeto11 <elteeto11@gmail.com>
2022-04-13 11:05:42 +0100

    feat(master): add logic to create user

--- src/user/backend/user.service.ts
+++ src/user/backend/user.service.ts
@@ -1,4 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { prisma, PrismaClient, User } from '@prisma/client';
+const primsa = new PrismaClient({
+    log: [
+        { level: 'query', emit: 'event' },
+        { level: 'info', emit: 'event' },
+        { level: 'error', emit: 'event' },
+      ],
+})
+
+primsa.$on('query', async (e) => {
+    console.log(`${e.query}`);
+  })
 
 @Injectable()
-export class UserService {}
+export class UserService {
+    constructor(){}
+    
+     async createUser({name,email}:{name:string,email:string}):Promise<User>{
+      return primsa.user.create({
+           data:{
+                name,
+                email
+           }
+       })
+    }
+}
