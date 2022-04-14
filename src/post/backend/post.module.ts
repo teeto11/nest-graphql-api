6317324233a331b2ffc1e48d22910182c2a29d20
teeto11 <elteeto11@gmail.com>
2022-04-14 09:42:17 +0100

    feat(master): export post module for backend

--- src/post/backend/post.module.ts
+++ src/post/backend/post.module.ts
@@ -1,4 +1,8 @@
 import { Module } from '@nestjs/common';
+import { PostService } from './post.service';
 
-@Module({})
+@Module({
+    providers:[PostService],
+    exports:[PostService],
+})
 export class PostModule {}
