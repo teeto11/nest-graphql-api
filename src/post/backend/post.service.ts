349ba69c4317c1da311fd61957f7675c966d169a
teeto11 <elteeto11@gmail.com>
2022-04-14 08:20:41 +0100

    feat(master): add logic to create post

--- src/post/backend/post.service.ts
+++ src/post/backend/post.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { prisma, PrismaClient, Post } from '@prisma/client';
+const primsa = new PrismaClient({
+    log: [
+        { level: 'query', emit: 'event' },
+        { level: 'info', emit: 'event' },
+        { level: 'error', emit: 'event' },
+      ],
+})
 
 @Injectable()
-export class PostService {}
+export class PostService {
+    async createPost({title,content,userId}:{title:string,content:string,userId:number}):Promise<Post>{
+        return primsa.post.create({
+            data:{
+                title,
+                content,
+                userId
+            }
+        })
+    }
+}
