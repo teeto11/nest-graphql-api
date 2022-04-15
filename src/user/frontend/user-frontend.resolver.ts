32ee2c5c5e1eb6bb6fd49d1fc7b721a62470782c
teeto11 <elteeto11@gmail.com>
2022-04-15 10:21:56 +0100

    feat(user-frontend): add query  to get a user, add resolve field query to get all user posts

--- src/user/frontend/user-frontend.resolver.ts
+++ src/user/frontend/user-frontend.resolver.ts
@@ -1,15 +1,26 @@
-import { Args, Mutation, Resolver } from "@nestjs/graphql";
+import { Args, Mutation, Parent, Query, ResolveField, Resolver } from "@nestjs/graphql";
+import { PostService } from "src/post/backend/post.service";
+import { Post } from "src/post/frontend/post";
 import { UserService } from "../backend/user.service";
+import { SingleUserFrontendInput } from "./single-user-frontend.input";
 import { User } from "./user";
-import { UserInput } from "./user-frontend.input";
+import { UserFrontendInput } from "./user-frontend.input";
 
-@Resolver()
+@Resolver(() => User)
 export class UserResolver{
-    constructor(private readonly userService:UserService){}
+    constructor(private readonly userService:UserService, private readonly postService:PostService){}
     
     @Mutation(() => User, {name:'createUser'})
-    async createUser(@Args('input')input:UserInput):Promise<User>{
+    async createUser(@Args('input')input:UserFrontendInput):Promise<User>{
         const user = await this.userService.createUser(input)
         return user
+    }
+    @Query(() => User, {name:'user'})
+    async getUser(@Args('input')input:SingleUserFrontendInput):Promise<User>{
+        return this.userService.getUser(input.id)
+    }
+    @ResolveField(() => [Post], {name:'posts'})
+    async getUserPosts(@Parent() user:User):Promise<Post[]>{
+        return this.postService.getUserPosts(user.id)
     }
 }
